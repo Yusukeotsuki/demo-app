3cc31f8eb91dceed39d0d01a0e74c6ca53f8d398
yusuke otsuki <yusuke.otsuki@accenture.com>
2025-05-20 16:35:34 +0900

    test

--- flask-vue-multiagent-demo/frontend/src/main.js
+++ flask-vue-multiagent-demo/frontend/src/main.js
@@ -1,11 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// Vuetify関係のインポート
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+
 const app = createApp(App)
 
 import('https://cdn.jsdelivr.net/npm/vuetify@3/dist/vuetify.esm.js')
   .then(({ createVuetify }) => {
-    const vuetify = createVuetify()
+    const vuetify = createVuetify({
+  components,
+  directives,
+})
     app.use(vuetify)
     app.mount('#app')
   })
